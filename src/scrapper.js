0afa5cb18f60b3fb9a2c4d7569b9b057f83b5e40
Steve Halford <steve@inknpixel.co.uk>
2013-07-23 22:00:44 +0100

    CSS3 only background

--- src/scrapper.js
+++ src/scrapper.js
@@ -1,24 +1,11 @@
 (function($) {
     $.scrapper = function(){
-        $('head').append('<link href="http://fonts.googleapis.com/css?family=Just+Another+Hand" rel="stylesheet" type="text/css">');
+        $('head').append('<link href="http://fonts.googleapis.com/css?family=Just+Another+Hand" rel="stylesheet" type="text/css">')
+                 .append('<link href="css/scrapper.css" rel="stylesheet">');
 
         $('.scrapper').each(function(index, node) {
             console.log(node);
-            $(node).css('background', 'red');
         });
-        $('.scrapper, .scrapper *')
-            .attr('style', "font-family: 'Just Another Hand', cursive !important");
-        $('.scrapper')
-            .css('padding', '4px')
-            .css('padding-left', '40px')
-            .css('padding-right', '20px')
-            .css('padding-bottom', '30px')
-            .css('padding-top', '25px')
-            .css('font-size', '1.4em')
-            .css('line-height', '1.5em')
-            .css('border', 'dashed 1px lightgray')
-            .css('background-image', "url('img/LinedPaperBackground.jpg')")
-        ;
     };
 })(jQuery);
 
